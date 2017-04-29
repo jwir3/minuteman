e60acc459a0d2b6b2cab5c0fa2dbf75eeb3c44c8
Scott Johnson <jaywir3@gmail.com>
2017-04-29 11:22:10 -0500

    Add basic functionality for Minutes' start times, along with tests.
    Refs #1.

--- src/Minutes.js
+++ src/Minutes.js
@@ -1,12 +1,39 @@
+'use strict';
+
+import moment from 'moment';
+
 /**
  * An object representing the minutes from a real-world meeting that either took
  * place in the past, or is taking place now.
  */
-function Minutes() {
+class Minutes {
+  constructor(scheduledStartTime) {
+    this.scheduledStartTime = scheduledStartTime;
+  }
+
+  get scheduledStartTime() {
+    return this.meetingScheduledTime;
+  }
+
+  set scheduledStartTime(time) {
+    if (time != null) {
+      this.meetingScheduledTime = time;
+    } else {
+      this.meetingScheduledTime = moment();
+    }
+  }
+
+  get scheduledStartTimeAsString() {
+    return this.formatDateTime(this.meetingScheduledTime);
+  }
+
+  formatDateTime(dateTime) {
+    if (dateTime == null) {
+      throw ("Unable to format a null date/time");
+    }
+
+    return dateTime.format('MMMM D, YYYY hh:mma');
+  }
 }
-
-Minutes.prototype = {
-
-};
 
 module.exports = Minutes;
---
+++ test/Minutes.spec.js
@@ -0,0 +1,25 @@
+import { should } from 'chai';
+import Minutes from '../src/Minutes';
+import moment from 'moment';
+
+should();
+
+describe('Minutes', () => {
+  describe('basic constructor', () => {
+    it('should construct an object of type Minutes', () => {
+      var minutes = new Minutes();
+      minutes.should.exist;
+    });
+  });
+
+  describe('constructor with a scheduled start time', () => {
+    it('should construct an object of type Minutes with a scheduled start time', () => {
+      var now = moment();
+      var minutes = new Minutes(now);
+      minutes.should.exist;
+
+      minutes.scheduledStartTime.should.equal(now);
+      minutes.scheduledStartTimeAsString.should.equal(now.format('MMMM DD, YYYY hh:mma'));
+    });
+  });
+});
